8dcd86c625540b1039ccf33f68ef8ff8bb8371b0
vilminessa <vilminessa@gmail.com>
2024-10-15 17:07:02 +0600

    Добавьте файлы проекта.

---
+++ WebBookstoreApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebBookstoreApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
